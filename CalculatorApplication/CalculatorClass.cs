414ed74749c54243c79b7ba819285e7d3dca7a4e
BRUH <BRUH@DESKTOP-5D5BOOT>
2024-09-11 02:23:41 +0800

    Add project files.

---
+++ CalculatorApplication/CalculatorClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApplication
+{
+    public delegate double Formula(double arg1, double arg2);
+    internal class CalculatorClass
+    {
+        private Formula _calculateEvent;
+        public event Formula CalculateEvent
+        {
+            add
+            {
+                _calculateEvent += value;
+                Console.WriteLine("Added the Delegate");
+            }
+            remove
+            {
+                _calculateEvent -= value;
+                Console.WriteLine("Removed the Delegate");
+            }
+        }
+        public double GetSum(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double GetDifference(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double GetProduct(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double GetQuotient(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return num1 / num2;
+        }
+    }
+}
